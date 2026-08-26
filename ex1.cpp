18ba08b4db2515138c6e6ae50cb7eddebe3b8252
SOSATYA THAT <sosatyathat2007@gmail.com>
2026-08-26 23:09:49 +0700

    EX1

---
+++ ex1.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+using namespace std; 
+int main() { 
+// TODO: print "Hello, my name is <your name>" 
+
+    std::cout << "Hello, my name is <your name>" << std::endl;
+    return 0; 
+} 
